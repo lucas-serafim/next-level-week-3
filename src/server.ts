4f61b46597f2d5353fba719ff3f6af9d6d15e440
lucas-serafim <lucas.serafim159@gmail.com>
2021-02-25 16:04:15 -0300

    Second day

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
+import 'reflect-metadata'
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-   return response.json({ message: "Hello"})
-})
+app.use(express.json());
+app.use(router);
 
-app.post("/", (request, response) => {
-   return response.json({ message: "Successful"})
-})
-
-app.listen(3000, () => console.log("Server is running!"))
+app.listen(3000, () => console.log("Server is running!"));
